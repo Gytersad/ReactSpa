a951ca7e6552592f0d0f08ce30590fd81137752d
Gytersad <gytersad07@gmail.com>
2020-10-24 17:47:10 +0300

    Разбитие на модули css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-
+import c from './NavBar.module.css';
 
 const NavBar = () => {
     return(
-        <nav className='nav'>
-            <ul>
-                <li><a href="#"></a>Profile</li>
-                <li><a href="#"></a>Messages</li>
-                <li><a href="#"></a>News</li>
-                <li><a href="#"></a>Music</li>
-                <li><a href="#"></a>Settings</li>
+        <nav className={c.nav}>
+            <ul className={c.menu}>
+                <li className={c.item}><a href="#"></a>Profile</li>
+                <li className={c.item}><a href="#"></a>Messages</li>
+                <li className={c.item}><a href="#"></a>News</li>
+                <li className={c.item}><a href="#"></a>Music</li>
+                <li className={c.item}><a href="#"></a>Settings</li>
             </ul>
         </nav>
     );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import c from './Profile.module.css';
 
 const Profile = () => {
     return(
-        <div className='content'>
+        <div className={c.content}>
             <h1>Main Content</h1>
             <img src="https://of-crimea.ru/plug/Peschanye-plyazhi-Kryma.jpg" alt=""/>
             <div>Ava +description</div>
@@ -12,9 +12,9 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>post 1</div>
-                    <div>post 2</div>
+                <div className={c.posts}>
+                    <div className={c.item}>post 1</div>
+                    <div className={c.item}>post 2</div>
                 </div>
             </div>
 
