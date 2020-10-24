0968252627c9be9e130049f8f0301a01f717bf31
GoldenBrown <32492024+Gytersad@users.noreply.github.com>
2020-10-24 15:46:24 +0300

    Add files via upload

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const NavBar = () => {
+    return(
+        <nav className='nav'>
+            <ul>
+                <li><a href="#"></a>Profile</li>
+                <li><a href="#"></a>Messages</li>
+                <li><a href="#"></a>News</li>
+                <li><a href="#"></a>Music</li>
+                <li><a href="#"></a>Settings</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <h1>Main Content</h1>
+            <img src="https://of-crimea.ru/plug/Peschanye-plyazhi-Kryma.jpg" alt=""/>
+            <div>Ava +description</div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Profile
